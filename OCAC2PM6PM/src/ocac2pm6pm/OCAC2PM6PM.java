5312ca034f73835a71266731eaa1f85b005d7f07
Satyaprakash80-cpu <satpathysatyaprakash699@gmail.com>
2024-07-09 16:13:09 +0530

    Add files via upload

---
+++ OCAC2PM6PM/src/ocac2pm6pm/OCAC2PM6PM.java
@@ -0,0 +1,21 @@
+A/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ocac2pm6pm;
+
+/**
+ *
+ * @author satyaprakas satpathy
+ */
+public class OCAC2PM6PM {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
